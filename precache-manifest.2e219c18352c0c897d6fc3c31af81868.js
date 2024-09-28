c53248c83c97eac799a4478ca52d9d3c90198813
cassano <gabriel_cassano@hotmail.com>
2024-09-28 21:21:53 +0200

    transfert application My Profile

---
+++ precache-manifest.2e219c18352c0c897d6fc3c31af81868.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f87f3cfeeffc6385b56b23e95d634f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e68825471ff700e75d4",
+    "url": "/static/css/2.b614aec9.chunk.css"
+  },
+  {
+    "revision": "07c13baac3a6d0171b1c",
+    "url": "/static/css/main.72c8927b.chunk.css"
+  },
+  {
+    "revision": "1e68825471ff700e75d4",
+    "url": "/static/js/2.46b3e2d4.chunk.js"
+  },
+  {
+    "revision": "07c13baac3a6d0171b1c",
+    "url": "/static/js/main.1f483e9b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
